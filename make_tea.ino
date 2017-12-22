00060f8cb0cb174a857c3df9c8e5a52d5585e65b
Brian Cline <standardhuman@gmail.com>
2017-12-22 15:59:34 -0800

    order of behavior is correct, runtime is compressed for testing/demo

--- make_tea.ino
+++ make_tea.ino
@@ -1,58 +1,89 @@
 #include <Servo.h>
 
 Servo servo;  // create servo object to control a servo
-int start_delay = 5000;
-int minutes_to_steep = .5;
-int servo_max_angle = 2; // max angle of the flag
-int servo_min_angle = 85; // min angle of the flag
+int minutes_to_steep = 1;
+int flag_up = 10;
+int flag_down = 90;
+int flag_half = 45;
+int flag_dunk_low = 65;
+int flag_dunk_high = 30;
+int raise_and_lower_duration = 8000;
 
-int flag_left = servo_max_angle + 5;
-int flag_right = servo_max_angle - 5;
-
+// starting values for loops
 int a = 0;
+int b = 0;
+int c = 0;
+int d = 0;
+int e = 0;
 int i = 0;
 int j = 0;
 int k = 0;
-int starttime = 0;
+int s = 0;
 
-void setup() {
-  servo.attach(9);  // attaches the servo on pin 9 to the servo object
-  servo.write(servo_max_angle); // set to min angle/lower tea bag.
-  delay(start_delay);
-}
-
-void loop() {
-  
-//  lower 
-    for(i = 0; servo.read() <= 85; i ++){
-        servo.write(i);
-        delay(75); 
-    }
-delay(5000);
-//  dunk/steep
-  for(a = 0; a <= minutes_to_steep * 2; a += 1){
-    for(j = 0; j <= 20; j += 1){
-        servo.write(servo_min_angle -= 1);
-        delay(350);
-    }
-    for(k = 0; k <= 20; k += 1){
-      servo.write(servo_min_angle += 1);
-      delay(350);
-    }
-    delay(3000);
+void slow_up() {
+  for (c = 0; servo.read() > flag_up; c++) {
+    servo.write(servo.read() - 1);
+    delay(50);
   }
-//   rise up
-servo.write(servo_max_angle);
-//shake
-delay(3000);
-for(i = 0; i <= 3; i += 1){
-    for(j = 0; j <= 5; j += 1){
-        servo.write(flag_left);
-        delay(50);
-        servo.write(flag_right);
-        delay(50);
-    }
-    delay(3000);
-   }
-   delay(160000);
 }
+void slow_down() {
+  for (d = 0; servo.read() < flag_down; d++) {
+    servo.write(servo.read() + 1);
+    delay(50);
+  }
+}
+
+void dunk(int steep_duration) {
+  double cycles = steep_duration / raise_and_lower_duration;
+  // dunk/steep
+  for (k = 0; k < cycles; k++) {
+    //raise
+    for (i = 0; servo.read() >= flag_dunk_high; i++) {
+      servo.write(servo.read() - 1);
+      delay(100);
+    }
+    delay(500);
+    //lower
+    for (j = 0; servo.read() <= flag_dunk_low; j++) {
+      servo.write(servo.read() + 1);
+      delay(100);
+    }
+  }
+  delay(5000);
+}
+
+void soak() {
+  for (e = 0; servo.read() <= flag_dunk_low; e++) {
+    servo.write(servo.read() + 1);
+    delay(100);
+  }
+}
+  void shake() {
+    for (s = 0; s <= 3; s++) {
+      for (b = 0; b <= 5; b++) {
+        servo.write(servo.read() - 10);
+        delay(100);
+        servo.write(servo.read() + 10);
+        delay(100);
+      }
+      delay(2000);
+    }
+  }
+
+  void setup() {
+    servo.attach(9);  // attaches the servo on pin 9 to the servo object
+
+    // setup() is probably not the proper place for this
+    slow_up();
+    delay(5000);
+    soak();
+    delay(5000);
+    dunk(35000);
+    slow_up();
+    delay(5000);
+    shake();
+  }
+
+  // master loop
+  void loop() {
+  }
